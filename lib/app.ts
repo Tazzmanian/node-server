01e96096b1ab2931e113107dc6269e90c5b94271
Tazzmanian <banskaliqtabg@yahoo.com>
2018-07-10 22:26:27 +0300

    add mongoose

--- lib/app.ts
+++ lib/app.ts
@@ -1,16 +1,24 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/crmRoutes";
+import * as mongoose from "mongoose";
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';  
 
     constructor() {
         this.app = express();
         this.config();        
         this.routePrv.routes(this.app);
+        this.mongoSetup();
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true });    
     }
 
     private config(): void{
